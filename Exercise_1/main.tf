bf4e19713506c47e0417379953caef66794e19bd
Miha <miha@rothl.com>
2021-04-19 19:33:36 +0100

    Terrform excercise is complete

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,32 @@
-# TODO: Designate a cloud provider, region, and credentials
+# Designate a cloud provider, region, and credentials
 
+provider "aws" {
+  profile = "default"
+  region = "eu-west-1"
+}
 
-# TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
+# provision 4 AWS t2.micro EC2 instances named Udacity T2
+resource "aws_instance" "UdacityT2" {
+  ami           = "ami-0ffea00000f287d30"
+  instance_type = "t2.micro"
+  
+  subnet_id     =  "subnet-b86a74ce"
 
+  count         = 4
+  tags = {
+    Name = "Udacity T2"
+  }
+}
 
-# TODO: provision 2 m4.large EC2 instances named Udacity M4
+# provision 2 m4.large EC2 instances named Udacity M4
+resource "aws_instance" "UdacityM4" {
+  ami           = "ami-0ffea00000f287d30"
+  instance_type = "m4.large"
+  
+  subnet_id     =  "subnet-b86a74ce"
+
+  count         = 2
+  tags = {
+    Name = "Udacity M4"
+  }
+}
--- Exercise_2/outputs.tf
+++ Exercise_2/outputs.tf
@@ -1,1 +1,6 @@
 # TODO: Define the output variable for the lambda function.
+
+output "lambda_invoke_arn" {
+  value       = "${aws_lambda_function.test_lambda.invoke_arn}"
+  description = "The ARN used to invoke the lambda function."
+}
