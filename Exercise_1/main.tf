6f4dadccc7118295e863bc5c02452926b106baf1
Miha <miha@rothl.com>
2021-04-20 06:57:05 +0100

    Update required changes.

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -18,15 +18,15 @@ resource "aws_instance" "UdacityT2" {
   }
 }
 
-# provision 2 m4.large EC2 instances named Udacity M4
-resource "aws_instance" "UdacityM4" {
-  ami           = "ami-0ffea00000f287d30"
-  instance_type = "m4.large"
-  
-  subnet_id     =  "subnet-b86a74ce"
-
-  count         = 2
-  tags = {
-    Name = "Udacity M4"
-  }
-}
+## provision 2 m4.large EC2 instances named Udacity M4
+#resource "aws_instance" "UdacityM4" {
+#  ami           = "ami-0ffea00000f287d30"
+#  instance_type = "m4.large"
+#  
+#  subnet_id     =  "subnet-b86a74ce"
+#
+#  count         = 2
+#  tags = {
+#    Name = "Udacity M4"
+#  }
+#}
